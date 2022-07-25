ea31b2cbe0c637f1b28bc87b821b7f6ebc94985c
ParkJeongSu <pjs91_marrow@naver.com>
2022-07-25 21:35:38 +0900

    초기 구조 설정

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Dashboard from './pages/main';
+function App() {
+  return (
+    <React.Fragment>
+      <Dashboard></Dashboard>
+    </React.Fragment>
+  );
+}
+
+export default App;
